ad405d233dee5b8cb46b058c076c21d21e43082b
anotherjsguy <kuldeepkeshwar@gmail.com>
2019-07-09 17:04:38 +0530

    avoid publishing in test environment

--- src/publish-size.js
+++ src/publish-size.js
@@ -2,7 +2,7 @@ const { repo, sha, event, branch, pull_request_number, ci } = require('ci-env');
 const axios = require('axios');
 const SIZE_STORE_ENDPOINT = process.env.SIZE_STORE_ENDPOINT || 'https://size-plugin-store.now.sh' ;
 async function publishDiff(diff) {
-	if (ci && event == 'pull_request') {
+	if (process.env.NODE_ENV !=='test' &&  ci && event == 'pull_request') {
 		try {
 			const params = { ci,repo, branch, sha, pull_request_number, diff };
 			await axios.post(`${SIZE_STORE_ENDPOINT}/diff`, params);
@@ -14,7 +14,7 @@ async function publishDiff(diff) {
 }
 async function publishSizes(size) {
 	// TODO: read allowed branch from configuration
-	if (ci && event == 'push' && branch==='master') {
+	if (process.env.NODE_ENV !=='test' &&  ci && event == 'push' && branch==='master') {
 		try {
 			const params = { ci,repo, branch, sha, pull_request_number, size };
 			await axios.post(`${SIZE_STORE_ENDPOINT}/size`, params);
